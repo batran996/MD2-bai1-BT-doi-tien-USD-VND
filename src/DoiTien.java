7aae295d47a9bed774992e342f84159686e6fc13
unknown <bazo20061996@gmail.com>
2022-07-24 23:40:39 +0700

    MD2-bai1-BT doi tien

---
+++ src/DoiTien.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DoiTien {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hãy nhập số USD bạn muốn  đổi nhé");
+        double usd = scanner.nextDouble();
+        double vnd = usd*23000;
+        System.out.printf("VND nhận được là " + vnd);
+    }
+}
